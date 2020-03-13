ba335c8047708b90cb47f10bf2fa0a1bfd24bf86
kitikil2 <kitikilabdelaziz@gmail.com>
2020-03-13 20:51:17 +0100

    Initialisation du tp gestion du departement

---
+++ src/main/java/org/tpe/gestiondep/GestiondepartementsApplication.java
@@ -0,0 +1,13 @@
+package org.tpe.gestiondep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondepartementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiondepartementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tpe/gestiondep/GestiondepartementsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tpe.gestiondep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondepartementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
