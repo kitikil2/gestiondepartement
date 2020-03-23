ed7bccb320b78932b7126f2cb6fa3429d7b577e6
kitikil2 <kitikilabdelaziz@gmail.com>
2020-03-23 15:34:07 +0100

    Test12

--- src/main/java/org/tpe/gestiodep/GestiondepartementsApplication.java
+++ src/main/java/org/tpe/gestiodep/GestiondepartementsApplication.java
@@ -1,6 +1,8 @@
 package org.tpe.gestiodep;
 
 
+import org.tpe.gestiodep.metier.ImplImetierInit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestiondepartementsApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private ImplImetierInit implImetierInit;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GestiondepartementsApplication.class, args);
@@ -16,6 +19,12 @@ public class GestiondepartementsApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		implImetierInit.initDepartement();
+		implImetierInit.initEtudiant();
+		implImetierInit.initEnseignant();
+		implImetierInit.initUniteEnseignement();
+		implImetierInit.initParcour();
+		implImetierInit.initNiveau();
 		System.out.println("Bonjour");
 	}
 
