4fa907a9bf21f32cd2d4e56afa27b334b8022e5b
kitikil2 <kitikilabdelaziz@gmail.com>
2020-03-23 22:09:06 +0100

    Metric CodeMR

---
+++ codemr/gestiondepartements/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"gestiondepartements", "Number of Highly Problematic Classes":0, "Number of Entities":16, "Number of Problematic Classes":0, "Class Lines of Code":381, "Number of External Packages":12, "Number of Packages":5, "Number of External Entities":26}
---
+++ codemr/gestiondepartements/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,17 @@
+var matrix = [[0,0,0,1,0],[0,0,0,0,0],[0,0,0,0,0],[0,1,1,0,0],[0,1,1,0,0]];
+var packages = [{
+"name": " org.tpe.gestiodep", "color": " #3182bd"
+}
+,{
+"name": " org.tpe.gestiodep.dao", "color": " #6baed6"
+}
+,{
+"name": " org.tpe.gestiodep.entitys", "color": " #9ecae1"
+}
+,{
+"name": " org.tpe.gestiodep.metier", "color": " #c6dbef"
+}
+,{
+"name": " org.tpe.gestiodep.service", "color": " #e6550d"
+}
+];
